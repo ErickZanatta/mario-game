3ddb9ba3dfa2b769abbf4cb50087fed11ab66048
ErickZanatta <107509687+ErickZanatta@users.noreply.github.com>
2022-06-15 18:39:29 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,84 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds')
+let score = document.querySelector("#score");
+const gameOver = document.querySelector('.gameOver')
 
-const jump = () => {
-  mario.classList.add('jump')
+// declarando uma variavel para o score
 
-  setTimeout(() => {
+let interval = null;
+let playerScore = 0;
 
-    mario.classList.remove('jump')
 
-  }, 500)
-
+// funçao para o score
+let scoreCounter = () => {
+  playerScore++;
+  score.innerHTML = `score <b>${playerScore}</b>`
 }
 
-const loop = setInterval(() => {
 
-  const pipePosition = pipe.offsetLeft;
-  const marioposition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-  
-  if (pipePosition <= 120 && pipePosition > 0 && marioposition < 150) {
+interval = setInterval(scoreCounter, 20)
+window.addEventListener("keydown" , (start)=>{
+  console.log(start)
+    if(start.code == "Space")
+      {
+            gameOver.style.display = "none";
+            pipe.classList.add("piperun")
+            clouds.classList.add("cloudsanimation")
+            
 
-    pipe.style.animation = 'none'
-    pipe.style.left = `${pipePosition}px`
+            let playerScore = 0;
+            interval = setInterval(scoreCounter, 20)
+      }
+})
 
-    mario.style.animation = 'none'
-    mario.style.bottom = `${marioposition}px`
+window.addEventListener("keydown" , (e)=>{
 
-    mario.src = "img/game-over.png"
-    mario.style.width = '75px'
-    mario.style.marginLeft = '40px'
+  if(e.key == "ArrowUp")
+      if(mario.classList != "jump")
+          {
+            mario.classList.add("jump")
+            setTimeout(()=>{
 
-    clearInterval(loop)
+                // remove class after 0.6 seconds
+                mario.classList.remove("jump")
 
-  }
+              },600)
+          }
 
-}, 10)
+})
 
-document.addEventListener('keydown', jump);
+
+
+let result = setInterval(()=>{
+    let marioposition = parseInt(getComputedStyle(mario).getPropertyValue("bottom"));
+    // console.log("mariobottom" + mariobottom)
+
+
+    let pipePosition = parseInt(getComputedStyle(pipe).getPropertyValue("left"));
+    // console.log("pipeLeft" + pipeLeft)
+
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioposition < 150){
+        // console.log("Game Over")
+
+        mario.src = "img/game-over.png"
+        mario.style.width = '75px'
+        mario.style.marginLeft = '40px'
+        mario.style.bottom = `${marioposition}px`
+
+        pipe.style.left = `${pipePosition}px`
+
+
+        mario.style.animation = 'none'
+        gameOver.style.display = "block";
+        pipe.classList.remove("piperun")
+        clouds.classList.remove("cloudsanimation")
+
+        clearInterval(interval);
+        let playerScore = 0;
+    }
+},10)
+
+
